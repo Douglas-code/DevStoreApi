ea2d09b52fed5c4d48f2f0b59ab59a9db7c3c71c
Douglas Rodrigues Pereira <douglasrodrigues20161@outlook.com>
2021-03-18 22:06:23 -0300

    Adicionando testes para os Value Objects

---
+++ DevStore.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,31 @@
+﻿using DevStore.Domain.StoreContext.ValueObjects;
+using Xunit;
+
+namespace DevStore.Tests.ValueObjects
+{
+    public class DocumentTests
+    {
+        private Document _validDocument;
+        private Document _invalidDocument;
+
+        public DocumentTests()
+        {
+            this._invalidDocument = new Document("111111111111");
+            this._validDocument = new Document("01140839080");
+        }
+
+        [Fact]
+        public void ShouldReturnNotificationWhenDocumentIsNotValid()
+        {
+            Assert.True(this._invalidDocument.Invalid);
+            Assert.True(this._invalidDocument.Notifications.Count > 0);
+        }
+
+        [Fact]
+        public void ShouldNotReturnNotificationWhenDocumentIsValid()
+        {
+            Assert.True(this._validDocument.Valid);
+            Assert.True(this._validDocument.Notifications.Count == 0);
+        }
+    }
+}
