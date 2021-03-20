4b75e2dd8d5afaa0829e51b75241f3be9b6ce5a3
Douglas Rodrigues Pereira <douglasrodrigues20161@outlook.com>
2021-03-20 14:35:46 -0300

    Testando Commands

---
+++ DevStore.Tests/Commands/CreateCustomerCommandTests.cs
@@ -0,0 +1,20 @@
+﻿using DevStore.Domain.StoreContext.Commands.CustomerCommads.Inputs;
+using Xunit;
+
+namespace DevStore.Tests.Commands
+{
+    public class CreateCustomerCommandTests
+    {
+        [Fact]
+        public void ShouldValidateWhenCommandIsVAlid()
+        {
+            CreateCustomerCommand command = new CreateCustomerCommand();
+            command.FirstName = "Douglas";
+            command.LastName = "Pereira";
+            command.Document = "58303366343";
+            command.Email = "dg@gmail.com";
+            command.Phone = "123123123";
+            Assert.True(command.Validate());
+        }
+    }
+}
