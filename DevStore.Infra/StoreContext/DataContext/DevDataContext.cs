a61820de33d5b79c8e5a8508677b5b8bdacf9b60
Douglas Rodrigues Pereira <douglasrodrigues20161@outlook.com>
2021-03-26 21:11:48 -0300

    Adicionando DataContext

---
+++ DevStore.Infra/StoreContext/DataContext/DevDataContext.cs
@@ -0,0 +1,24 @@
+﻿using DevStore.Shared;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DevStore.Infra.StoreContext.DataContext
+{
+    public class DevDataContext : IDisposable
+    {
+        public SqlConnection Connection { get; set; }
+
+        public DevDataContext()
+        {
+            this.Connection = new SqlConnection(Settings.ConnectionString);
+            this.Connection.Open();
+        }
+
+        public void Dispose()
+        {
+            if (this.Connection.State != ConnectionState.Closed)
+                this.Connection.Close();
+        }
+    }
+}
---
+++ DevStore.Shared/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace DevStore.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = "server=localhost,1433;database=DevStore;User ID=SA;Password=#Askisu0107";
+    }
+}
