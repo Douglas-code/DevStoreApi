bba8c6ec38570f9be83af09449b8b6ae60523003
Douglas Rodrigues Pereira <douglasrodrigues20161@outlook.com>
2021-03-31 21:09:44 -0300

    Adicionando EmailService

---
+++ DevStore.Infra/StoreContext/Services/EmailService.cs
@@ -0,0 +1,12 @@
+﻿using DevStore.Domain.StoreContext.Services;
+
+namespace DevStore.Infra.StoreContext.Services
+{
+    public class EmailService : IEmailService
+    {
+        public void Send(string to, string from, string subject, string body)
+        {
+            // Todo -> Implementar
+        }
+    }
+}
